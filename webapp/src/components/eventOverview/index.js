882765c4fdb1627791d3acae9664aa9a8be96872
Edgar Dorausch <EdgarDorausch@web.de>
2017-12-04 22:51:38 +0100

    Update event overview

--- webapp/src/components/eventOverview/index.js
+++ webapp/src/components/eventOverview/index.js
@@ -3,21 +3,21 @@ import React from 'react';
 import {Grid, Rating} from 'semantic-ui-react';
 
 // Import resources
-import Slider from '../slider/imageSlider';
+import ImageSlider from '../slider/imageSlider';
 
 
-const EventOverview = () => (
+const EventOverview = ({eventName, date, rating, description, categories}) => (
         <div>
             <Grid>
                 <Grid.Row>
                     <Grid.Column width="8">
-                        <Slider/>
+                        <ImageSlider />
                     </Grid.Column>
                     <Grid.Column width="8">
-                        <h2>DasSuperEvent</h2>
-                        <h3 style={{color: 'gray'}}>29. September 2018 (Samstag)</h3>
-                        <Rating defaultRating={3} maxRating={5} disabled />
-                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                        <h2>{eventName}</h2>
+                        <h3 style={{color: 'gray'}}>{date}</h3>
+                        <Rating defaultRating={rating} maxRating={5} disabled />
+                        <p>{description}</p>
                     </Grid.Column>
                 </Grid.Row>
             </Grid>

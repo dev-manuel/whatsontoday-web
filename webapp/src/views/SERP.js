45651bad20dacecd7638b97e3ec4fb91c90aedc6
Edgar Dorausch <EdgarDorausch@web.de>
2017-12-19 19:47:48 +0100

    Restructured big event tile table code

--- webapp/src/views/SERP.js
+++ webapp/src/views/SERP.js
@@ -6,11 +6,27 @@ import './SERP.less'
 import FilterPanel from '../components/filterPanel';
 import EventTileTableBig from '../components/eventTileTableBig';
 
+
+const LoremIpsum = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
+
+const eventList = [
+    {name: 'StartUpWeekend', date: '17. Nov. 2017', categories: ['#Business'], description: LoremIpsum, imageURI:'#', target: '#'},
+    {name: 'StartUpWeekend', date: '17. Nov. 2017', categories: ['#Business'], description: LoremIpsum, imageURI:'#', target: '#'},
+    {name: 'StartUpWeekend', date: '17. Nov. 2017', categories: ['#Business'], description: LoremIpsum, imageURI:'#', target: '#'},
+    {name: 'StartUpWeekend', date: '17. Nov. 2017', categories: ['#Business'], description: LoremIpsum, imageURI:'#', target: '#'},
+    {name: 'StartUpWeekend', date: '17. Nov. 2017', categories: ['#Business'], description: LoremIpsum, imageURI:'#', target: '#'},
+    {name: 'StartUpWeekend', date: '17. Nov. 2017', categories: ['#Business'], description: LoremIpsum, imageURI:'#', target: '#'},
+    {name: 'StartUpWeekend', date: '17. Nov. 2017', categories: ['#Business'], description: LoremIpsum, imageURI:'#', target: '#'},
+    {name: 'StartUpWeekend', date: '17. Nov. 2017', categories: ['#Business'], description: LoremIpsum, imageURI:'#', target: '#'},
+    {name: 'StartUpWeekend', date: '17. Nov. 2017', categories: ['#Business'], description: LoremIpsum, imageURI:'#', target: '#'},
+    
+]
+
 const SERP = () => (
         <div className="pageContent">
             <FilterPanel/>
             <div className="tileTable">
-                <EventTileTableBig/>
+                <EventTileTableBig eventList={eventList}/>
             </div>
         </div>
 )

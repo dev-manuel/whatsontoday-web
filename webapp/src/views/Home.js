b69ba0436600ed17b991332d42908255b2915752
Edgar Dorausch <EdgarDorausch@web.de>
2017-12-19 19:03:05 +0100

    Restructured category tile table code

--- webapp/src/views/Home.js
+++ webapp/src/views/Home.js
@@ -6,11 +6,21 @@ import Header from '../components/header';
 import Slider from '../components/slider';
 import CategoryTileTable from '../components/categoryTileTable';
 import EvenTileTable from '../components/eventTileTable';
+import exampleTileImage from '../img/example_tile.png';
+
+
+const CategoryList = [
+    {name: 'StartUp', imageURI: exampleTileImage, target: '#'},
+    {name: 'Wissenschaft', imageURI: exampleTileImage, target: '#'},
+    {name: 'Party', imageURI: exampleTileImage, target: '#'},
+    {name: 'Feste', imageURI: exampleTileImage, target: '#'},
+    {name: 'Kultur', imageURI: exampleTileImage, target: '#'},
+]
 
 const Home = () => (
         <div>
             <Slider title='Education'/>
-            <CategoryTileTable/>
+            <CategoryTileTable categoryList={CategoryList}/>
             <EvenTileTable/>
         </div>
 )

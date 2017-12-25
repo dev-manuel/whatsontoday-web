51b66b5156c666ae03ae71936cbdf5e84f72066f
Frithjof Winkelmann <frithjof97@web.de>
2017-12-25 19:55:38 +0100

    Added check for develop branch

--- travis/success.sh
+++ travis/success.sh
@@ -1,11 +1,19 @@
-cd $TRAVIS_BUILD_DIR
-sbt swagger
-git clone git://${GH_REPO} $HOME/$REPO
-cp $TRAVIS_BUILD_DIR/target/swagger/swagger.json $HOME/$REPO/rest_doc
-cd $HOME/$REPO/rest_doc
-git remote
-git config user.email ${EMAIL}
-git config user.name ${USER}
-git add swagger.json
-git commit -m "Travis auto-commit"
-git push "https://${GH_TOKEN}@${GH_REPO}" master > /dev/null 2>&1
+echo $TRAVIS_BRANCH
+if [ $TRAVIS_BRANCH = "develop" ]
+then
+    echo "Updating api documentation..."
+    cd $TRAVIS_BUILD_DIR
+    sbt swagger
+    git clone git://${GH_REPO} $HOME/$REPO
+    cp $TRAVIS_BUILD_DIR/target/swagger/swagger.json $HOME/$REPO/rest_doc
+    cd $HOME/$REPO/rest_doc
+    git remote
+    git config user.email ${EMAIL}
+    git config user.name ${USER}
+    git add swagger.json
+    git commit -m "Travis auto-commit"
+    git push "https://${GH_TOKEN}@${GH_REPO}" master > /dev/null 2>&1
+    echo "Done!"
+else
+    echo "Not updating api documentation because this is not a build of the develop branch"
+fi

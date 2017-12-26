a95a1fddfa3741c9c6bec2a49d8ba8e7a398ebc5
Edgar Dorausch <EdgarDorausch@web.de>
2017-12-26 21:34:22 +0100

    Beautified nav bar layout

--- webapp/src/components/header/index.js
+++ webapp/src/components/header/index.js
@@ -1,6 +1,6 @@
 // Import modules
 import React from 'react';
-import {Menu, Search, Icon, Dropdown} from 'semantic-ui-react';
+import {Menu, Search, Icon, Dropdown, Container} from 'semantic-ui-react';
 
 // Import resources
 import logo from '../../img/logo1.jpg';
@@ -8,40 +8,56 @@ import PopupMenu from './PopupMenu';
 import './menu.less'
 
 const Header = () => (
-        <Menu className='headerMenu' borderless>
-            <Menu.Item>
+        <Menu borderless className='headerMenu'>
+            <Menu.Item className='headerSpacer'/>
+
+            <Menu.Item className='headerLogo'>
                 <img src={logo} />
             </Menu.Item>
 
-            <Menu.Item>
+            <Menu.Item className='headerSearch'>
                 <Search/>
             </Menu.Item>
 
-            <Menu.Item position='right'>
-                 <a>
+            <Menu.Item className='headerButtons'>
+                 <a 
+                    className='headerButtonStyle'
+                    href='#'
+                 >
                     <Icon name='newspaper' />
                     Blog
                 </a>
 
-                <a>
+                <a 
+                    className='headerButtonStyle'
+                    href='#'
+                 >
                     <Icon name='plus' />
                     Add Event
                 </a>
 
-                <a>
+                <a 
+                    className='headerButtonStyle'
+                    href='#'
+                 >
                     <Icon name='sign in' />
                     Sign In
                 </a>
 
-                <a>
+                <a 
+                    className='headerButtonStyle'
+                    href='#'
+                 >
                     <Icon name='signup' />
                     Sign Up
                 </a>
             </Menu.Item>
 
-            <Menu.Item>
+            <Menu.Item className='headerPopup'>
                 <PopupMenu/>
             </Menu.Item>  
+            
+            <Menu.Item className='headerSpacer'/>
         </Menu>
 )
 

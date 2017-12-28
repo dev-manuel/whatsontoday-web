94bd918d6add91c756078879b3bdcac03d1e7a85
Edgar Dorausch <EdgarDorausch@web.de>
2017-12-28 20:04:03 +0100

    Updated popup/dropdown menu

--- webapp/src/components/header/index.js
+++ webapp/src/components/header/index.js
@@ -1,14 +1,13 @@
 // Import modules
 import React from 'react';
-import {Menu, Search, Icon, Button} from 'semantic-ui-react';
+import {Menu, Search, Icon, Button, Dropdown} from 'semantic-ui-react';
 
 // Import resources
 import logo from '../../img/logo1.jpg';
-import PopupMenu from './PopupMenu';
 import './menu.less'
 
 const Header = () => (
-        <Menu borderless className='headerMenu'>
+        <Menu borderless className='headerMenu' size='large'>
             <Menu.Item className='headerSpacer'/>
 
             <Menu.Item className='headerLogo'>
@@ -22,76 +21,48 @@ const Header = () => (
 
             <Menu.Item className='headerButtons'>
 
-                <Button 
-                    className='headerButtonStyle'
-                    basic
-                    color='teal'                    
-                >
-                    <Icon name='newspaper' /> Blog
-                </Button>
+                    <Button 
+                        className='headerButtonStyle'
+                        basic
+                        color='teal'                    
+                    >
+                        <Icon name='newspaper' /> Blog
+                    </Button>
 
-                <Button 
-                    className='headerButtonStyle'
-                    basic
-                    color='teal'
-                >
-                    <Icon name='plus' /> Add Event
-                </Button>
+                    <Button 
+                        className='headerButtonStyle'
+                        basic
+                        color='teal'
+                    >
+                        <Icon name='plus' /> Add Event
+                    </Button>
 
-                <Button 
-                    className='headerButtonStyle'
-                    basic
-                    color='teal'
-                >
-                    <Icon name='sign in' /> Sign In
-                </Button>
+                    <Button 
+                        className='headerButtonStyle'
+                        basic
+                        color='teal'
+                    >
+                        <Icon name='sign in' /> Sign In
+                    </Button>
 
-                <Button 
-                    className='headerButtonStyle'
-                    basic
-                    color='teal'
-                >
-                    <Icon name='signup' /> Sign Up
-                </Button>
+                    <Button 
+                        className='headerButtonStyle'
+                        basic
+                        color='teal'
+                    >
+                        <Icon name='signup' /> Sign Up
+                    </Button>
 
 
-                 {/* <a 
-                    className='headerButtonStyle'
-                    href='#'
-                 >
-                    <Icon name='newspaper' />
-                    Blog
-                </a>
-
-                <a 
-                    className='headerButtonStyle'
-                    href='#'
-                 >
-                    <Icon name='plus' />
-                    Add Event
-                </a>
-
-                <a 
-                    className='headerButtonStyle'
-                    href='#'
-                 >
-                    <Icon name='sign in' />
-                    Sign In
-                </a>
-
-                <a 
-                    className='headerButtonStyle'
-                    href='#'
-                 >
-                    <Icon name='signup' />
-                    Sign Up
-                </a> */}
-            </Menu.Item>
-
-            <Menu.Item className='headerPopup'>
-                <PopupMenu/>
+                    <Dropdown icon='sidebar' pointing className='item headerDropdownButton'>
+                        <Dropdown.Menu>
+                            <Dropdown.Item>Lorem</Dropdown.Item>
+                            <Dropdown.Item>Ipsum</Dropdown.Item>
+                            <Dropdown.Item>Dolor</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
             </Menu.Item>  
-            
+
             <Menu.Item className='headerSpacer'/>
         </Menu>
 )

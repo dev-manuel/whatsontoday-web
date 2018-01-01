e4301db109e1fd5e943316136fd09488c69f2659
Edgar Dorausch <EdgarDorausch@web.de>
2018-01-01 18:02:17 +0100

    Added Axios to the project

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -2,13 +2,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route} from 'react-router';
-import {HashRouter, IndexRoute} from 'react-router-dom'
+import {HashRouter, IndexRoute} from 'react-router-dom';
+import axios from 'axios';
 
 // Import resources
 import BaseView from './views/BaseView';
 import 'semantic-ui-less/semantic.less';
 import 'slick-carousel/slick/slick.less';
 import 'slick-carousel/slick/slick-theme.less';
+import { error } from 'util';
+
+export const AXIOS = axios.create({
+    baseURL: 'http://localhost:9000/api/', // Just for dev!
+    timeout: 10000
+});
 
 
 // Setup of local page routing

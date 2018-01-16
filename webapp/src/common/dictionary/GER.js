bbbd1ff4ef22105d744040ab1ea16f6d4530d473
Edgar Dorausch <EdgarDorausch@web.de>
2018-01-16 22:45:23 +0100

    Added dictionary

---
+++ webapp/src/common/dictionary/GER.js
@@ -0,0 +1,65 @@
+
+export const GER = {
+
+    header: {
+        blog: 'Blog',
+        addEvent : 'Event hinzufügen',
+        signIn: 'Anmelden',
+        signUp: 'Registrieren',
+        // dropdown...
+    },
+    home: {
+        startUp: 'StartUp',
+        science: 'Wissenschaft',
+        party: 'Party',
+        culture: 'Kultur',
+        fair: 'Feste'
+    },
+    footer: {
+        socialMedia: 'Social Media',
+        facebook: 'Facebook',
+        twitter: 'Twitter',
+        instagram: 'Instagram',
+
+        about: 'Über uns',
+        contactUs: 'Kontakt',
+        theTeam: 'Das Team',
+        faq: 'FAQ',
+
+        slogan: 'Finde dein nächstes Event',
+        terms: 'AGB',
+        privacyPolicy: 'Datenschutzerklärung'
+    },
+    serp:{
+        what: 'Was?',
+        when: 'Wann?',
+        where: 'Wo?',
+    },
+    event: {
+        what: 'Was?',
+        when: 'Wann?',
+        where: 'Wo?',
+        cost: 'Kosten',
+        moreInformation: 'Weitere Informationen',
+        moreAbout: (x) => `Mehr zu ${x}`,
+        contact: (x) => `${x} kontaktieren`,
+
+        also: 'Diese Events könnten dir auch gefallen',
+    },
+    signUp: {
+        message: 'Registriere dich bei What\'s On',
+        email: 'E-Mail Adresse',
+        password: 'Passwort',
+        passwordRepeat: 'Wiederhole das Passwort',
+        agree: 'Ich akzieptiere die Nutzungsbedingungen',
+        submit: 'Los Geht\'s'
+    },
+    signIn: {
+        email: 'E-Mail Adresse',
+        password: 'Passwort',
+        remember: 'Eingeloggt bleiben',
+        submit: 'Los Geht\'s',
+        newToUs: 'Neu bei uns?',
+        signUp: 'Registriere dich!'
+    }
+}

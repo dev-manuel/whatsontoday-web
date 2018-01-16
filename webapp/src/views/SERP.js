ca549ea3deb9e618c59504a0220d5402e9f29cc5
Edgar Dorausch <EdgarDorausch@web.de>
2018-01-16 01:09:21 +0100

    SERP-View code restructuring

--- webapp/src/views/SERP.js
+++ webapp/src/views/SERP.js
@@ -5,6 +5,8 @@ import React from 'react';
 import './SERP.less'
 import FilterPanel from '../components/filterPanel';
 import EventTileTableBig from '../components/eventTileTableBig';
+import StatefulView from '../common/StatefulView';
+import AbstractViewState from '../common/AbstractViewState';
 
 
 const LoremIpsum = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
@@ -22,13 +24,69 @@ const eventList = [
     
 ]
 
-const SERP = () => (
-        <div className="pageContent">
-            <FilterPanel/>
-            <div className="tileTable">
-                <EventTileTableBig eventList={eventList}/>
-            </div>
-        </div>
-)
+export default class SERP extends StatefulView{
+    constructor(props){
+        super(props);
 
-export default SERP;
+        this.state = {
+            viewState: new ShowingState(this),
+        }
+    }
+}
+
+//
+// ─── VIEW-STATES ─────────────────────────────────────────────────────────────────
+//
+
+// Todo
+/**
+ * @override
+ */
+class LoadingState extends AbstractViewState{
+
+    constructor(context){
+        super(context);
+    }
+
+    render() {
+        return (
+            <div style={{marginLeft: '11%', marginRight: '11%'}}>
+                This View is loading!
+            </div>
+        )
+    }
+}
+
+/**
+ * @override
+ */
+class ShowingState extends AbstractViewState{
+    render(){
+        return (
+            <div className="pageContent">
+                <FilterPanel/>
+                <div className="tileTable">
+                    <EventTileTableBig eventList={eventList}/>
+                </div>
+            </div>
+        )
+    }
+}
+
+// Todo
+/**
+ * @override
+ */
+class ErrorState extends AbstractViewState{
+    constructor(context){
+        super(context);
+    }
+
+    render() {
+        return (
+            <div style={{marginLeft: '11%', marginRight: '11%'}}>
+                An error occurred!
+            </div>
+        )
+    }
+}

c28f6267927f0e261316bb1e3cea0ef9bd6f5310
Frithjof Winkelmann <frithjof97@web.de>
2018-01-24 16:40:09 +0100

    Added coverage reports to github page

--- travis/success.sh
+++ travis/success.sh
@@ -5,11 +5,13 @@ then
     sbt swagger
     git clone git://${GH_REPO} $HOME/$REPO
     cp $TRAVIS_BUILD_DIR/target/swagger/swagger.json $HOME/$REPO/rest_doc
-    cd $HOME/$REPO/rest_doc
+    cp -a $TRAVIS_BUILD_DIR/target/scala-2.12/scoverage-report/ $HOME/$REPO/coverage/
+    cd $HOME/$REPO
     git remote
     git config user.email ${EMAIL}
     git config user.name ${USER}
-    git add swagger.json
+    git add rest_doc/swagger.json
+    git add coverage/
     git commit -m "Travis auto-commit"
     git push "https://${GH_TOKEN}@${GH_REPO}" master > /dev/null 2>&1
     echo "Done!"

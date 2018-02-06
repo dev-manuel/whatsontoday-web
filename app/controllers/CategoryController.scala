9dcbd1f04ca48bd7bee6fb4854b71b9e8148110a
Frithjof Winkelmann <frithjof97@web.de>
2018-02-06 16:37:04 +0100

    Added POST Request for Categories

--- app/controllers/CategoryController.scala
+++ app/controllers/CategoryController.scala
@@ -10,14 +10,23 @@ import play.api.mvc._
 import slick.jdbc.JdbcProfile
 import whatson.db._
 import whatson.model.detail.CategoryDetail._
+import whatson.service._
+import whatson.model._
+import com.mohiva.play.silhouette.api._
+import whatson.auth._
+import whatson.db.Util._
 
 /**
  * This Controller handles API Requests concerning categories
  */
-class CategoryController @Inject()(cc: ControllerComponents, protected val dbConfigProvider: DatabaseConfigProvider)
+class CategoryController @Inject()(cc: ControllerComponents,
+                                   protected val dbConfigProvider: DatabaseConfigProvider,
+                                   val silhouette: Silhouette[AuthEnv],
+                                   val organizerService: OrganizerService,
+                                   val userService: UserService)
     (implicit context: ExecutionContext)
     extends AbstractController(cc)
-    with HasDatabaseConfigProvider[JdbcProfile]{
+    with HasDatabaseConfigProvider[JdbcProfile] with Util {
 
   val log = Logger("api.categories")
 
@@ -27,5 +36,13 @@ class CategoryController @Inject()(cc: ControllerComponents, protected val dbCon
     val q = for(e <- CategoryTable.category) yield e;
 
     db.run(q.detailed).map(x => Ok(Json.toJson(x)))
+  }
+
+  def createCategory() = organizerRequest(parse.json(Category.categoryReads)) { case (request,organizer) =>
+    log.debug("Rest request to create category")
+
+    val inserted = db.run(insertAndReturn[Category,CategoryTable](CategoryTable.category,request.body))
+
+    inserted.map(x => Ok(Json.toJson(x)))
   }
 }
--- test/controllers/CategoryControllerSpec.scala
+++ test/controllers/CategoryControllerSpec.scala
@@ -6,15 +6,43 @@ import play.api.test._
 import play.api.test.Helpers._
 import whatson.util._
 import scala.concurrent._
+import play.api.libs.json._
+import play.api.mvc._
+import whatson.model._
+import scala.concurrent.duration._
 
 class CategoryControllerSpec extends RestTestSuite {
 
   "CategoryController GET" should {
 
     "return a list of categories" in {
-      val categories = route(app, FakeRequest(GET, "/api/v1/categories")).get
+      val categories = route(app, FakeRequest(GET, "/api/v1/category")).get
 
       status(categories) mustBe OK
+    }
+  }
+
+  "CategoryController POST" should {
+    "return OK on proper request from organizer" in {
+      val organizer = Await.result(createOrganizer(), Duration.Inf)
+      val category = Category(None, "testcategory", 1)
+
+      val create = route(app, FakeRequest(POST, "/api/v1/category",
+                                          new Headers(List(("Content-Type","application/json"),("x-auth-token",organizer._3))),
+                                          Json.toJson(category))).get
+
+      status(create) mustBe OK
+    }
+
+    "return Unauthorized on request from user" in {
+      val user = Await.result(createUser(), Duration.Inf)
+      val category = Category(None, "testcategory", 1)
+
+      val create = route(app, FakeRequest(POST, "/api/v1/category",
+                                          new Headers(List(("Content-Type","application/json"),("x-auth-token",user._3))),
+                                          Json.toJson(category))).get
+
+      status(create) mustBe UNAUTHORIZED
     }
   }
 }

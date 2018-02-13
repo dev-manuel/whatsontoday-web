9f8be4a3c9e775bbe58e324c05bee313fd0967db
Edgar Dorausch <EdgarDorausch@web.de>
2018-02-13 19:06:10 +0100

    Moved signIn api request

--- webapp/src/common/api.js
+++ webapp/src/common/api.js
@@ -71,5 +71,20 @@ export const api = {
                     break;
                 }
         })
+    },
+
+    /**
+     * @param {string} email
+     * @param {string} password
+     * @param {boolean} rememberMe
+     */
+    signIn: (email, password, rememberMe) => {
+        return axios.post('/login/signIn', {
+            email,
+            password,
+            rememberMe,
+        }).then(response => {
+                return response.data.token;
+        })
     }
 }

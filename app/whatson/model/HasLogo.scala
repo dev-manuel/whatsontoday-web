409b3530e8bfb5ba4e09791a82dc5d3fdb0e712d
Frithjof Winkelmann <frithjof97@web.de>
2018-02-18 22:59:10 +0100

    Removed unused classes

--- app/whatson/model/HasLogo.scala
+++
@@ -1,8 +0,0 @@
-package whatson.model
-
-import whatson.model._
-import whatson.model.detail._
-
-trait HasLogo {
-  val logo: ImageDetail
-}
--- app/whatson/model/UserType.scala
+++
@@ -1,7 +0,0 @@
-package whatson.model
-
-import whatson.db._
-
-object UserType extends DbEnumeration {
-  val User, Organizer = Value
-}

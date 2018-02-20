9a26e3a46037c0b9d9d52ce9b983f37814371c98
Edgar Dorausch <EdgarDorausch@web.de>
2018-02-21 00:36:08 +0100

    Added location requests

---
+++ webapp/src/common/api/requests/location.js
@@ -0,0 +1,63 @@
+import {axios} from '../index'
+
+/**
+ * 
+ * @param {string} name 
+ * @param {number} [xPage=0]
+ * @param {number} [xPageSize=20]
+ */
+export const getLocations = (name, xPage = 0, xPageSize = 20) => {
+    return axios.get('/location',{
+        headers: {
+            "X-Page": xPage,
+            "X-Page-Size": xPageSize,
+        },
+    }).then( response => {
+        return response.data;
+    })
+}
+
+/**
+ * 
+ * @param {number} id 
+ */
+export const readLocation = id => {
+    return axios.get(`/locations/${id}`)
+        .then( response => {
+            return response.data;
+        })
+}
+
+/**
+ * 
+ * @param {number} id 
+ * @param {number} xPage 
+ * @param {number} xPageSize 
+ */
+export const getNearbyLocations = (id, xPage = 0, xPageSize = 20) => {
+    return axios.get(`locations/nearby/${id}`, {
+        headers: {
+            "X-Page": xPage,
+            "X-Page-Size": xPageSize,
+        },
+    })
+        .then( response => {
+            return response.data;
+        })
+}
+
+/**
+ * 
+ * @param {string} name 
+ * @param {number} latitude 
+ * @param {number} longitude 
+ */
+export const createLocation = (name, latitude, longitude) => {
+    return axios.post('/locations', {
+        name,
+        latitude,
+        longitude,
+      }).then( response => {
+          return response.data;
+      })
+}

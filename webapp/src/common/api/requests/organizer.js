2f019f21e5ebfbb5a9ad8fd55670267cd433867d
Edgar Dorausch <EdgarDorausch@web.de>
2018-02-21 01:19:43 +0100

    Updated organizer requests

--- webapp/src/common/api/requests/organizer.js
+++ webapp/src/common/api/requests/organizer.js
@@ -6,9 +6,19 @@ import {axios} from '../index'
  * @param {string} name
  */
 export const organizerSignUp = (email, password, name) => {
-    return axios.post('/user/signUp', {
+    return axios.post('/organizer/signUp', {
         email,
         password,
         name,
     })
+}
+
+/**
+ * @param {number} id 
+ */
+export const readOrganizer = id => {
+    return axios.get(`/organizer/${id}`)
+        .then( response => {
+            return response.data;
+        })
 }

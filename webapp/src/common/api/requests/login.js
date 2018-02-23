dc3ae01db81f9593654628b874d63ad539acba0c
Edgar Dorausch <EdgarDorausch@web.de>
2018-02-23 15:02:15 +0100

    Updated login requests

--- webapp/src/common/api/requests/login.js
+++ webapp/src/common/api/requests/login.js
@@ -15,3 +15,57 @@ export const signIn = (email, password, rememberMe) => {
             return response.data.token;
     })
 }
+
+/**
+ * 
+*/
+export const signOut = () => {
+    return axios.get('/login/signOut/')
+        .then( response => {
+            return response.data;
+        })
+}
+
+/**
+ * 
+*/
+export const getLoggedInUser = () => {
+    return axios.get('/login')
+        .then( response => {
+            return response.data;
+        })
+}
+
+/**
+ * 
+ * @param {string} newPassword 
+ */
+export const updatePassword = newPassword => {
+    return axios.put('/login', {
+        password: newPassword,
+    }).then( response => {
+        return response.data;
+    })
+}
+
+/**
+ * 
+ * @param {string} email 
+ */
+export const resetPassword = email => {
+    return axios.put('/login/resetPassword', {
+        email,
+    }).then( response => {
+        return response.data;
+    })
+}
+
+/**
+ * 
+*/
+export const deleteUser = () => {
+    return axios.delete('/login')
+        .then( response => {
+            return response.data;
+        })
+}

426c9f09f9f5cfe53c25f376801649c9cafaab3b
Edgar Dorausch <EdgarDorausch@web.de>
2018-03-07 23:38:22 +0100

    Beautified event tiles in the SERP

--- webapp/src/components/eventTileTableBig/eventTileBig.js
+++ webapp/src/components/eventTileTableBig/eventTileBig.js
@@ -1,60 +1,50 @@
 import React from 'react'
-import { Icon, Image as ImageComponent, Item, Segment } from 'semantic-ui-react'
+import {Icon} from 'semantic-ui-react'
+import {Link} from 'react-router-dom'
 
 import {stringifyTime} from '../../common/timeStringification'
 import exampleTileImage from '../../img/example_tile.png'
 
 import './eventTileBig.less'
 
-/**
- * 
- * @param {{name: string, from: Date, to: Date categories: string, description: string, imageURI: string, target: string }} props 
- */
-const ItemExampleExtraContent = ({name, from, to, categories, description, imageURI, target, language}) => {
-    const stringifiedTime = stringifyTime(from, to, language.time);
-
-    return(
-        <Segment>
-            <Item.Group href={target}>
-                <Item>
-                <Item.Image size='small' src={exampleTileImage}/>
-
-                <Item.Content>
-                    <Item.Header>
-                        {name}
-
-                        {/* Display the dates */}
-                        <Icon name='calendar' />
-                        <div className="eventOverviewDate">
-                            {stringifiedTime.firstLine}
-                            <br/>
-                            {stringifiedTime.secondLine}
-                        </div>
-                    </Item.Header>
-                    <Item.Description>{description}</Item.Description>
-                    <Item.Extra>
-                        <span style={{float: 'right'}}><Icon color="gray" name="tags" /> {categories}</span>
-                    </Item.Extra>
-                </Item.Content>
-                </Item>
-            </Item.Group>
-        </Segment>
-    )
-}
 
 export default ({name, from, to, categories, description, imageURI, target, language}) => {
     const {firstLine, secondLine} = stringifyTime(from, to, language.time);    
 
     return (
-        <div className="eventTileBig_main">
-            <img src={exampleTileImage} className="eventTileBig_image"/>
-            <div className="eventTileBig_body">
-                <div className="eventTileBig_heading">{name}</div>
-                <div className="eventTileBig_date">
-                    <div className="fistLine">{firstLine}</div>
-                    <div className="secondLine">{secondLine}</div>
+        <Link to={target}>
+            <div className="eventTileBig_main">
+                <img src={exampleTileImage} className="eventTileBig_image"/>
+                <div className="eventTileBig_body">
+                
+                    <div className="eventTileBig_sectionTop">
+                        <div className="eventTileBig_heading">{name}</div>
+                        <div className="eventTileBig_date">
+                            <div className="fistLine">{firstLine}</div>
+                            <div className="secondLine">{secondLine}</div>
+                        </div>
+                    </div>
+
+                    <div className="eventTileBig_sectionMiddle">
+                        <div className="eventTileBig_description">
+                            {description}
+                        </div>
+                    </div>
+
+                    <div className="eventTileBig_sectionBottom">
+                        {/* TODO! */}
+                        {/* <div className="eventTileBig_price">
+                            {'LoremIpsum'} 
+                        </div> */}
+                        <div className="eventTileBig_categories">
+                            {/* TODO: Use Max/Min number of categories */}
+                            <Icon name="tags" /> 
+                            {'LoremIpsum'}
+                        </div>
+                    </div>
+
                 </div>
             </div>
-        </div>
+        </Link>
     )
 }

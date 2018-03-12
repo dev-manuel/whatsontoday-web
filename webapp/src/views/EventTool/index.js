5bcc4ba6a7da456c27186f25d54123059f14ca4c
Edgar Dorausch <EdgarDorausch@web.de>
2018-03-12 22:27:41 +0100

    Added Event tool base structure (routing)

---
+++ webapp/src/views/EventTool/index.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import {Route, Switch} from 'react-router-dom'
+
+import Overview from './overview'
+import Create from './create'
+import Update from './update'
+import Delete from './delete'
+import NotFound from '../404'
+
+
+export const eventToolLinks = {
+    create: 'create',
+    update: 'update',
+    delete: 'delete',
+}
+
+export default ({match, language, setLoginData}) => {
+    const basePath = match.path;
+    return (
+        <Switch>
+            {/* Overview */}
+            <Route
+                exact
+                path={`${basePath}/`}
+                render={routeProps => (
+                    <Overview
+                        {...routeProps}
+                        language={language}
+                    />
+                )}
+            />
+            
+            {/* Create */}
+            <Route
+                path={`${basePath}/${eventToolLinks.create}`}
+                render={routeProps => (
+                    <Create
+                        {...routeProps}
+                        language={language}
+                    />
+                )}
+            />
+            
+            {/* Update */}
+            <Route
+                path={`${basePath}/${eventToolLinks.update}`}
+                render={routeProps => (
+                    <Update
+                        {...routeProps}
+                        language={language}
+                    />
+                )}
+            />
+
+            {/* Delete */}
+            <Route
+                path={`${basePath}/${eventToolLinks.delete}`}
+                render={routeProps => (
+                    <Delete
+                        {...routeProps}
+                        language={language}
+                    />
+                )}
+            />
+
+            {/* Delete */}
+            <Route
+                path={`${basePath}/${eventToolLinks.delete}`}
+                render={routeProps => (
+                    <Delete
+                        {...routeProps}
+                        language={language}
+                    />
+                )}
+            />
+
+            {/* NotFound */}
+            <Route
+                path='/*'
+                render={routeProps => (
+                    <NotFound
+                        {...routeProps}
+                        language={language}
+                    />
+                )}
+            />
+            
+        </Switch>
+    )
+}

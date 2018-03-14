6f86bee1d9e0e79f5f9aa37034aa06b2f7c50011
Edgar Dorausch <EdgarDorausch@web.de>
2018-03-14 02:44:42 +0100

    Added base file table

--- webapp/src/views/EventTool/create.js
+++ webapp/src/views/EventTool/create.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import {Segment, Container, Header, Divider, Dropdown, Form, Button} from 'semantic-ui-react'
+import {Segment, Container, Header, Divider, Dropdown, Form, Button, Table, Icon, Loader} from 'semantic-ui-react'
 import DatePicker from 'react-datepicker'
 import moment from 'moment'
 import log from 'loglevel'
 
 import {getLocations} from '../../common/api/requests/location'
+import {uploadImage} from '../../common/api/requests/image'
 
 import 'react-datepicker/dist/react-datepicker.css'
 import './EventTool.less'
@@ -119,6 +120,21 @@ export class LocationSelector extends React.Component {
     }
 }
 
+/**
+ * @readonly
+ * @enum {string}
+ */
+export const FileEntryStatus = {
+    LOCAL: 'local',
+    LOADING: 'loading',
+    UPLOADED: 'uploaded',
+}
+
+/**
+ * @typedef {{status: FileEntryStatus, file: File, key: number}} FileEntry
+ * @typedef {[FileEntry]} FileEntryList
+ */
+
 export class ImageUploadFormField extends React.Component {
 
     componentDidMount(){
@@ -154,6 +170,63 @@ export class ImageUploadFormField extends React.Component {
     }
 }
 
+class FileTable extends React.Component {
+
+    getIconByFileEntryStatus(status){
+        switch(status){
+            case FileEntryStatus.LOCAL:
+                return <Icon name='close' color='red' size='large'/>
+            break;
+
+            case FileEntryStatus.LOADING:
+                return <Loader active inline size='small'/>
+            break;
+
+            case FileEntryStatus.UPLOADED:
+                return <Icon name='checkmark' color='green' size='large'/>
+            break;
+
+            default:
+            break;
+        }
+    }
+
+    render(){
+        /**
+         * @type {FileEntryList}
+         */
+        const fileEntryList = this.props.fileEntryList;
+        log.debug('FileTable#fileEntryList', fileEntryList);
+
+        
+
+        return (
+            <Table fixed>
+
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>FileName</Table.HeaderCell>
+                        <Table.HeaderCell>Is Uploaded</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+        
+                <Table.Body>
+                    {
+                        fileEntryList.map((fileEntry, index) => (
+                            <Table.Row key={index}>
+                                <Table.Cell>{fileEntry.file.name}</Table.Cell>
+                                <Table.Cell>
+                                    {this.getIconByFileEntryStatus(fileEntry.status)}
+                                </Table.Cell>
+                            </Table.Row>
+                        ))
+                    }
+                </Table.Body>
+            </Table>
+        )
+    }
+}
+
 
 export default class Create extends React.Component {
 
@@ -163,10 +236,55 @@ export default class Create extends React.Component {
         from: null,
         to: moment.now(),
         location: {},
-        description: ''
+        description: '',
+        /**
+         * @type {FileEntry}
+         */
+        thumbnailImage: null,
+        /**
+         * @type {FileEntryList}
+         */
+        sliderImages: [],
     }
 
+
+    handleSliderImageSelection(event, files){
+        log.debug('ImageUploadFormField#event', event);
+        log.debug('ImageUploadFormField#files', files);
+
+        const sliderImages = Array.from(files).map( (file, index) => ({
+            status: FileEntryStatus.LOADING,
+            key: index,
+            file,
+        }));
+
+        this.setState({
+            sliderImages,
+        })
+
+        sliderImages.forEach(fileEntry => {
+            uploadImage(fileEntry.file, `img${Math.floor(Math.random()*1000000)}`)
+                .then(data => {
+                    this.setState((prevState, props) => {
+
+                        const index = prevState.sliderImages.findIndex(entry => entry.key===fileEntry.key);
+                        if(index === -1){ // Not found
+                        } else {
+                            const newSliderImages = prevState.sliderImages;
+                            newSliderImages[index].status = FileEntryStatus.UPLOADED;
+                            return {
+                                sliderImages: newSliderImages,
+                            }
+                        }
+                    })
+                })
+                .catch(error => {
+                    log.debug('handleSliderImageSelection#error', error);
+                })
+        })
+    }
     
+
     render(){
         const categoryOptions = [{
             key: 'party',
@@ -205,10 +323,11 @@ export default class Create extends React.Component {
 
                         <ImageUploadFormField
                             text='Add Images'
-                            onChange={(event, files) => {
-                                log.debug('ImageUploadFormField#event', event);
-                                log.debug('ImageUploadFormField#files', files);
-                            }}
+                            onChange={this.handleSliderImageSelection.bind(this)}
+                        />
+                        
+                        <FileTable
+                            fileEntryList={this.state.sliderImages}
                         />
                        
 

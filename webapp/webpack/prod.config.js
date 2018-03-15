32f9b080a16dd3828c5b09b2e7a0b882f778f6b1
Edgar Dorausch <EdgarDorausch@web.de>
2018-03-15 18:37:06 +0100

    Updated default value restriction for webpack prod config

--- webapp/webpack/prod.config.js
+++ webapp/webpack/prod.config.js
@@ -15,15 +15,15 @@ const log = require('loglevel');
 module.exports = (env = {}) => { 
     console.log('Webpack env settings:', env);
 
-    if(!env.API_BASE_PATH){
-        throw new Error(`API_BASE_PATH environment variable is not defined but needed! Please specify your API base path with npm run build -- --env.API_BASE_PATH=<...>`);
+    if(!env.API_BASE_URL){
+        throw new Error(`API_BASE_URL environment variable is not defined but needed! Please specify your API base path with npm run build -- --env.API_BASE_URL=<...>`);
     }
 
     const definitions = {
         'process.env.NODE_ENV': JSON.stringify('production'), 
-        DISABLE_PRIVATE_ROUTES: JSON.stringify(false), // To ensure this property is set to false
-        LOG_LEVEL: JSON.stringify(log.levels.SILENT),   
-        API_BASE_PATH: JSON.stringify(env.API_BASE_PATH),
+        DISABLE_PRIVATE_ROUTES: JSON.stringify(env.DISABLE_PRIVATE_ROUTES || false),
+        LOG_LEVEL: JSON.stringify(env.LOG_LEVEL || log.levels.SILENT), 
+        API_BASE_URL: JSON.stringify(env.API_BASE_URL),
     }
     console.log('Webpack definitions:', definitions);
 

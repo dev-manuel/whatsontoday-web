e696c27af20fb088a2b3946d3fc2de1d9bc32749
Edgar Dorausch <EdgarDorausch@web.de>
2018-03-18 03:46:06 +0100

    Updated user SignUp view

--- webapp/src/views/SignUp/user.js
+++ webapp/src/views/SignUp/user.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import {Grid} from 'semantic-ui-react'
+
+import Successful from './successful';
+import UserSignUpForm from './components/userSignUpForm'
+
 
 export default class User extends React.Component {
+
+    state={
+        showSuccessView: false,
+    }
+
+    onSuccess(){
+        this.setState({
+            showSuccessView: true,
+        })
+    }
+
     render(){
-        return (
-            'TODO'
-        )
+        return this.state.showSuccessView ?
+            <Successful 
+            language={this.props.language} /> :
+            (
+                <div className='login-form'>
+                <Grid
+                    textAlign='center'
+                    verticalAlign='middle'
+                >
+                    <Grid.Column style={{ maxWidth: 450 }}>
+                        <UserSignUpForm 
+                            onSuccess={this.onSuccess.bind(this)}
+                            language={this.props.language}
+                        />
+                    </Grid.Column>
+                </Grid>
+            </div>
+            )
     }
 }
+

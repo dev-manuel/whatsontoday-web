95f8512c7bb6808f155f055de2d73c0eae692dda
Edgar Dorausch <EdgarDorausch@web.de>
2018-03-19 15:04:16 +0100

    Update travis config

--- travis/build.sh
+++ travis/build.sh
@@ -1,6 +1,6 @@
 cd ./webapp/ &&
 npm install &&
-npm test &&
+npm testCov &&
 npm run build -- --env.API_BASE_URL=http://localhost:9000 &&
 cd ../ &&
 sbt ++$TRAVIS_SCALA_VERSION coverage test coverageReport swagger

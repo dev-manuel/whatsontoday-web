c7a4d9da74ffe5f40f3e2038bb8e5532c1369167
Edgar Dorausch <EdgarDorausch@web.de>
2018-03-23 00:28:42 +0100

    Added social media bar to base view

---
+++ webapp/src/views/BaseView/components/socialMediaBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Button} from 'semantic-ui-react'
+
+import './socialMediaBar.less'
+
+export default class SocialMediaBar extends React.Component {
+    render(){
+        const lang = this.props.language.externalLinks;
+        const socialMediaSize = 'huge';
+        return (
+            <div className="BaseView_socialMediaBar">
+                <div className="BaseView_buttonContainer">
+                    <Button href={lang.facebook} size={socialMediaSize} circular color='facebook' icon='facebook f' />
+                    <Button href={lang.twitter} size={socialMediaSize} circular color='twitter' icon='twitter' />
+                    <Button href={lang.instagram} size={socialMediaSize} circular color='instagram' icon='instagram' />
+                </div>
+            </div>
+        )
+    }
+}

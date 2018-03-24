270cdfb5fa3485e9962c5f895d45a8e95e5f7bee
Frithjof Winkelmann <frithjof97@web.de>
2018-03-24 17:48:39 +0100

    Updated deploy.sh

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,13 +1,15 @@
 echo "Hello from deploy.sh"
 
 cd $TRAVIS_BUILD_DIR
-cd webapp
-npm install
-npm run build -- --env.API_BASE_URL=https://www.whats-on.today:3443/api/v1 --env.LOG_LEVEL=debug
-cd ..
-sbt dist
-export SSHPASS=$DEPLOY_PASS
-sshpass -e scp -o stricthostkeychecking=no ./target/universal/whatson-1.0-SNAPSHOT.zip $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+#cd webapp
+#npm install
+#npm run build -- --env.API_BASE_URL=https://www.whats-on.today:3443/api/v1 --env.LOG_LEVEL=debug
+#cd ..
+#sbt dist
+#export SSHPASS=$DEPLOY_PASS
+#sshpass -e scp -o stricthostkeychecking=no ./target/universal/whatson-1.0-SNAPSHOT.zip $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+#sshpass -e scp -o stricthostkeychecking=no ./target/universal/whatson-1.0-SNAPSHOT.zip $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/start.sh
 
 if [ $TRAVIS_BRANCH = "master" ]
 then

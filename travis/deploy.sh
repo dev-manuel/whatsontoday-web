7cd2ce29515461fe7072bac90b2364176d18faee
Frithjof Winkelmann <frithjof97@web.de>
2018-03-24 17:32:43 +0100

    Updated deploy script

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -6,7 +6,8 @@ npm install
 npm run build -- --env.API_BASE_URL=https://www.whats-on.today:3443/api/v1 --env.LOG_LEVEL=debug
 cd ..
 sbt dist
-sshpass -p ${SSH_ROOT} scp ./target/universal/whatson-1.0-SNAPSHOT.zip root@whats-on.today:~
+export SSHPASS=$DEPLOY_PASS
+sshpass -e scp -o stricthostkeychecking=no ./target/universal/whatson-1.0-SNAPSHOT.zip $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
 
 if [ $TRAVIS_BRANCH = "master" ]
 then

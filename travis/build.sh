36b0a1e4f284e4669078061fc3a94eb06e4d9573
Frithjof Winkelmann <frithjof97@web.de>
2018-03-24 18:20:01 +0100

    Updated build & deployment

--- travis/build.sh
+++ travis/build.sh
@@ -1,6 +1,6 @@
-#cd ./webapp/ &&
-#npm install &&
-#npm run testCov &&
-#npm run build -- --env.API_BASE_URL=http://localhost:9000 &&
-#cd ../ &&
-#sbt ++$TRAVIS_SCALA_VERSION coverage test coverageReport swagger
+cd ./webapp/ &&
+npm install &&
+npm run testCov &&
+npm run build -- --env.API_BASE_URL=http://localhost:9000 &&
+cd ../ &&
+sbt ++$TRAVIS_SCALA_VERSION coverage test coverageReport swagger
--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,16 +1,17 @@
 echo "Hello from deploy.sh"
 
-cd $TRAVIS_BUILD_DIR
-cd webapp
-npm install
-npm run build -- --env.API_BASE_URL=https://www.whats-on.today:3443/api/v1 --env.LOG_LEVEL=debug
-cd ..
-sbt dist
-export SSHPASS=$DEPLOY_PASS
-sshpass -e scp -o stricthostkeychecking=no ./target/universal/whatson-1.0-SNAPSHOT.zip $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
-sshpass -e ssh -o stricthostkeychecking=no $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/start.sh
-
 if [ $TRAVIS_BRANCH = "master" ]
 then
     echo "Deploying new version to server"
+    
+    cd $TRAVIS_BUILD_DIR
+    cd webapp
+    npm install
+    npm run build -- --env.API_BASE_URL=https://www.whats-on.today:3443/api/v1 --env.LOG_LEVEL=debug
+    cd ..
+    sbt dist
+    export SSHPASS=$DEPLOY_PASS
+    sshpass -e scp -o stricthostkeychecking=no ./target/universal/whatson-1.0-SNAPSHOT.zip $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+    sshpass -e ssh -o stricthostkeychecking=no $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/start.sh
+
 fi

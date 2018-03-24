f6974f125050facdee63f2b663a5bd53562fd039
Frithjof Winkelmann <frithjof97@web.de>
2018-03-24 17:57:53 +0100

    Updated deploy.sh

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -9,7 +9,7 @@ cd $TRAVIS_BUILD_DIR
 export SSHPASS=$DEPLOY_PASS
 #sshpass -e scp -o stricthostkeychecking=no ./target/universal/whatson-1.0-SNAPSHOT.zip $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
 #sshpass -e scp -o stricthostkeychecking=no ./target/universal/whatson-1.0-SNAPSHOT.zip $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
-sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/start.sh
+sshpass -e ssh -o stricthostkeychecking=no $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/start.sh
 
 if [ $TRAVIS_BRANCH = "master" ]
 then

846b3947cf74287fe46058d46739f99c99cc6cb4
Frithjof Winkelmann <frithjof97@web.de>
2018-03-27 00:32:34 +0200

    Added Play Error handler

---
+++ app/controllers/ErrorHandler.scala
@@ -0,0 +1,33 @@
+package controllers
+
+import scala.concurrent.ExecutionContext
+import play.api.http.HttpErrorHandler
+import play.api.mvc._
+import play.api.mvc.Results._
+import scala.concurrent._
+import javax.inject.Singleton
+import javax.inject._
+import play.api.http.FileMimeTypes
+import whatson.util._
+
+
+@Singleton
+class ErrorHandler @Inject()(implicit context: ExecutionContext,
+                             val fileMimeTypes: FileMimeTypes,
+                             applicationConfig: ApplicationConfig)
+    extends HttpErrorHandler {
+
+  def onClientError(request: RequestHeader, statusCode: Int, message: String) = {
+    if(statusCode==404)
+      Future.successful(Redirect(applicationConfig.url ++ "web/404"))
+    else
+      Future.successful(
+        Status(statusCode)("A client error occurred: " + message)
+      )
+  }
+
+  def onServerError(request: RequestHeader, exception: Throwable) = {
+    Future.successful(Redirect(applicationConfig.url ++ "web/500"))
+    //TODO: Inform devs?
+  }
+}

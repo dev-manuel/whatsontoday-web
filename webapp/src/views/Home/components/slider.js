e8fab6b6c9948e5882f1a6c9d6c70a2f487db5a1
Edgar Dorausch <EdgarDorausch@web.de>
2018-04-02 20:23:32 +0200

    Added slider arrows

--- webapp/src/views/Home/components/slider.js
+++ webapp/src/views/Home/components/slider.js
@@ -5,7 +5,37 @@ import Slick from 'react-slick';
 // Import resources
 import './slider.less';
 
+const NextArrow = ({onClick}) => {
+	return (
+		<div
+			className="Home_slider_nextContainer"
+			onClick={onClick}
+		>
+			<div className="nextIcon"/>
+		</div>
+	)
+}
+
+const BackArrow = ({onClick}) => {
+	return (
+		<div
+			className="Home_slider_backContainer"
+			onClick={onClick}			
+		>
+			<div className="backIcon"/>
+		</div>
+	)
+}
+
 class Slider extends React.Component {
+
+	back(){
+		this.slider.slickPrev();
+	}
+
+	next(){
+		this.slider.slickNext();
+	}
 
     render() {
         const settings = {
@@ -22,10 +52,19 @@ class Slider extends React.Component {
 		} = this.props;
 
         return (
-          	<div className="sliderContainer">
-            	<Slick {...settings}>
+          	<div className="slider_Container">
+				<Slick
+					ref={ slider => {this.slider = slider}}
+					{...settings}
+				>
 					{slides.map((slide, index) => (<div key={index}>{slide}</div>))}
             	</Slick>
+				<NextArrow 
+				  	onClick={this.next.bind(this)}
+				/>
+				<BackArrow
+				  	onClick={this.back.bind(this)}					
+				/>
          	</div>
         )
     }

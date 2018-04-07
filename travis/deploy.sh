3ad972f2658598582fe341b2b42d837e223facf5
Frithjof Winkelmann <frithjof97@web.de>
2018-04-07 13:22:10 +0200

    Setup deployment of test server on development branch

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -14,3 +14,19 @@ then
     sshpass -e scp -o stricthostkeychecking=no ./target/universal/whatson-1.0-SNAPSHOT.zip $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
     sshpass -e ssh -o stricthostkeychecking=no $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/start.sh
 fi
+
+if [[ $TRAVIS_BRANCH = "develop"  &&  $TRAVIS_PULL_REQUEST = false ]]
+then
+    echo "Deploying new test version to server"
+
+    cd $TRAVIS_BUILD_DIR
+    cd webapp
+    npm install
+    npm run build -- --env.API_BASE_URL=https://www.whats-on.today:3443/api/v1 --env.LOG_LEVEL=debug
+    cd ..
+    sbt dist
+    export SSHPASS=$DEPLOY_PASS
+    mv ./target/universal/whatson-1.0-SNAPSHOT.zip ./target/universal/whatson-1.0-QA.zip
+    sshpass -e scp -o stricthostkeychecking=no ./target/universal/whatson-1.0-QA.zip $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+    sshpass -e ssh -o stricthostkeychecking=no $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/qa.sh
+fi
